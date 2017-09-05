08a8dd3be3f1933988a7c81012686d75447d7faf
squeemishly <squeemishly@gmail.com>
2017-09-05 09:32:41 -0600

    pulls pasted in text and adds it to the right side of the screen. sizes the text by the frequency of it's occurance in the pasting.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const Words = require('./words')
+const $ = require('jquery')
+
 
 document.addEventListener("DOMContentLoaded", () => {
   Words.topWord()
+  $('.btn-break-down').on("click", Words.showWordFrequency)
 })
--- src/words.js
+++ src/words.js
@@ -13,6 +13,25 @@ class Words {
   static getTopWord() {
     return $.getJSON(`https://wordwatch-api.herokuapp.com//api/v1/top_word`)
   }
+
+  static showWordFrequency() {
+    const allWords = $('.text-to-break-down').val().split(/[ :;,-.\]\[\n)(]+/)
+    const wordCount = {}
+    allWords.forEach((word) => {
+      word = word.toLowerCase()
+      if (wordCount[word]) {
+        wordCount[word] = wordCount[word] + 1
+      } else {
+        wordCount[word] = 1
+      }
+    })
+    for (var property in wordCount) {
+      if (wordCount.hasOwnProperty(property)) {
+        $('.word-count').append(`<span style="font-size:${wordCount[property]}em">${property}</span>`)
+      }
+    }
+    // debugger
+  }
 }
 
 module.exports = Words
