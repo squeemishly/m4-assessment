2ff889029c555b3c5dc6bb157df813f857b13e91
Chris Brown <chris.brown@skookum.com>
2017-09-05 13:55:30 -0600

    Be wary of `class`.
    I have issues with `class` in JavaScript and avoid it whenever I can:
    
    1. It's misleading. JavaScript has no classical inheritance.
    
    2. It often requires somebody using your class to instantiate it with
    the `new` keyword, and it they don't, it can have unintended
    consequences.
    
    In this case, I think it's just the wrong abstraction. You aren't using
    a constructor, or extending a base class, or doing anything that would
    necessitate a class at all. Even more telling, all of your methods are
    static. I think what you want is just an object literal containing a few
    functions. This, in my experience, is true 95% of the time. Using an
    object literal is more _correct_ most of time.

--- src/words.js
+++ src/words.js
@@ -2,45 +2,45 @@ const $ = require('jquery')
 const htmlHelper = require('./htmlHelper')
 const wordWatchApiCalls = require('./wordWatchApiCalls')
 
-class Words {
-  static topWord() {
+const Words = {
+  topWord () {
     wordWatchApiCalls.getTopWord()
     .then((word) => {
       const topWord = Object.keys(word.word)[0];
       const numTimes = word.word[topWord];
       htmlHelper.appendTopWord(topWord, numTimes);
     })
-  }
+  }, 
 
-  static showWordFrequency() {
+  showWordFrequency () {
     const allWords = htmlHelper.getAllWords();
     const wordCount = {};
     Words.createWordCount(allWords, wordCount);
     Words.addWordsToWordCount(wordCount);
     htmlHelper.resetTextEntry();
-  }
+  },
 
-  static createWordCount(allWords, wordCount) {
+  createWordCount (allWords, wordCount) {
     allWords.forEach((word) => {
       word = word.toLowerCase();
       wordCount[word] = wordCount[word] + 1 || 1;
     })
-  }
+  },
 
-  static addWordsToWordCount(wordCount) {
+  addWordsToWordCount (wordCount) {
     for (var property in wordCount) {
       if (wordCount.hasOwnProperty(property)) {
         htmlHelper.addWordToWordCount(wordCount, property);
         wordWatchApiCalls.postTextToAPI(property);
       }
     }
-  }
+  },
 
-  static pressEnterToWord(e) {
+  pressEnterToWord (e) {
     if (e.keyCode == 13) {
         Words.showWordFrequency();
     }
   }
 }
 
-module.exports = Words
+module.exports = Words;
