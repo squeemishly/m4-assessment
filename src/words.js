d8c30625e3b8d79559a1a5837f3cda549cf31ec8
Chris Brown <chris.brown@skookum.com>
2017-09-05 13:51:48 -0600

    Use semicolons!
    Statements look incomplete or unfinished without semicolons. Omitting
    them makes it harder to read your code.

--- src/words.js
+++ src/words.js
@@ -6,39 +6,39 @@ class Words {
   static topWord() {
     wordWatchApiCalls.getTopWord()
     .then((word) => {
-      const topWord = Object.keys(word.word)[0]
-      const numTimes = word.word[topWord]
-      htmlHelper.appendTopWord(topWord, numTimes)
+      const topWord = Object.keys(word.word)[0];
+      const numTimes = word.word[topWord];
+      htmlHelper.appendTopWord(topWord, numTimes);
     })
   }
 
   static showWordFrequency() {
-    const allWords = htmlHelper.getAllWords()
-    const wordCount = {}
-    Words.createWordCount(allWords, wordCount)
-    Words.addWordsToWordCount(wordCount)
-    htmlHelper.resetTextEntry()
+    const allWords = htmlHelper.getAllWords();
+    const wordCount = {};
+    Words.createWordCount(allWords, wordCount);
+    Words.addWordsToWordCount(wordCount);
+    htmlHelper.resetTextEntry();
   }
 
   static createWordCount(allWords, wordCount) {
     allWords.forEach((word) => {
-      word = word.toLowerCase()
-      wordCount[word] = wordCount[word] + 1 || 1
+      word = word.toLowerCase();
+      wordCount[word] = wordCount[word] + 1 || 1;
     })
   }
 
   static addWordsToWordCount(wordCount) {
     for (var property in wordCount) {
       if (wordCount.hasOwnProperty(property)) {
-        htmlHelper.addWordToWordCount(wordCount, property)
-        wordWatchApiCalls.postTextToAPI(property)
+        htmlHelper.addWordToWordCount(wordCount, property);
+        wordWatchApiCalls.postTextToAPI(property);
       }
     }
   }
 
   static pressEnterToWord(e) {
     if (e.keyCode == 13) {
-        Words.showWordFrequency()
+        Words.showWordFrequency();
     }
   }
 }
